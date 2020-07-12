be46ff9033ece940d07562403696e4c79709b4d5
Leda Liang <ledaliang@wustl.edu>
2020-07-11 19:20:14 -0500

    stl aldermen spider

--- city_scrapers/spiders/stl_aldermen.py
+++ city_scrapers/spiders/stl_aldermen.py
@@ -1,12 +1,11 @@
-from datetime import datetime, timedelta
 import re
 from collections import defaultdict
+from datetime import datetime
 
 import scrapy
-from city_scrapers_core.constants import COMMITTEE, BOARD, NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD, COMMITTEE, NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
-from dateutil.relativedelta import relativedelta
 
 
 class StlAldermenSpider(CityScrapersSpider):
@@ -15,11 +14,12 @@ class StlAldermenSpider(CityScrapersSpider):
     timezone = "America/Chicago"
     custom_settings = {"ROBOTSTXT_OBEY": False}
     start_urls = [
-        "https://www.stlouis-mo.gov/government/departments/aldermen/aldermanic-legislative-session.cfm"
+        (
+            "https://www.stlouis-mo.gov/government/departments/"
+            "aldermen/aldermanic-legislative-session.cfm"
+        )
     ]
 
-    
-    
     def __init__(self, *args, **kwargs):
         self.agenda_map = defaultdict(list)
         super().__init__(*args, **kwargs)
@@ -30,11 +30,16 @@ class StlAldermenSpider(CityScrapersSpider):
 
     def _parse_meetings_page(self, response):
         urls = [
-            "https://www.stlouis-mo.gov/events/past-meetings.cfm?span=-30&department=332",
-            "https://www.stlouis-mo.gov/events/all-public-meetings.cfm?span=30"
+            (
+                "https://www.stlouis-mo.gov/events/"
+                "past-meetings.cfm?span=-30&department=332"
+            ),
+            "https://www.stlouis-mo.gov/events/all-public-meetings.cfm?span=30",
         ]
         for url in urls:
-            yield scrapy.Request(url=url, method="GET", callback=self._parse_events_page)
+            yield scrapy.Request(
+                url=url, method="GET", callback=self._parse_events_page
+            )
 
     def _parse_events_page(self, response):
         for url in self._get_event_urls(response):
@@ -58,7 +63,7 @@ class StlAldermenSpider(CityScrapersSpider):
         """
         start = self._parse_start(response)
         links_key = datetime.strftime(start, "%m-%d-%y")
-        
+
         meeting = Meeting(
             title=self._parse_title(response),
             description=self._parse_description(response),
@@ -78,7 +83,7 @@ class StlAldermenSpider(CityScrapersSpider):
 
         meeting["status"] = self._get_status(meeting)
         meeting["id"] = self._get_id(meeting)
-        yield meeting
+        return meeting
 
     def _parse_title(self, response):
         """Parse or generate meeting title."""
@@ -86,17 +91,19 @@ class StlAldermenSpider(CityScrapersSpider):
         title = title.replace("Meeting", "").replace("Metting", "")
         title = title.replace("-", "- ")
         title = title.replace("(Canceled)", "Cancelled")
-        return title.replace("  ", " ")
+        return title.replace("  ", " ").strip()
 
     def _parse_description(self, response):
         """Parse or generate meeting description."""
-        description = response.css("div#EventDisplayBlock div.col-md-8 h4 strong::text").getall()
+        description = response.css(
+            "div#EventDisplayBlock div.col-md-8 h4 strong::text"
+        ).getall()
         i = 0
         while i < len(description) - 1:
             if "following:" in description[i]:
-                return description[i+1].replace("\xa0","")
+                return description[i + 1].replace("\xa0", "")
             elif "will" in description[i]:
-                return description[i].replace("\xa0","")
+                return description[i].replace("\xa0", "")
             else:
                 i += 1
         else:
@@ -115,9 +122,10 @@ class StlAldermenSpider(CityScrapersSpider):
     def _parse_start(self, response):
         """Parse start datetime as a naive datetime object."""
         date = response.css("div.page-title-row p.page-summary::text").get()
-        pattern = r"(?P<day>\d{2}/\d{2}/\d{2}), (?P<time>(\d{1,2}:\d{2}) (PM|AM)) - (\d{1,2}:\d{2}) (PM|AM)"
+        pattern = r"(?P<day>\d{2}/\d{2}/\d{2}), (?P<time>(\d{1,2}:\d{2}) (PM|AM))"
+        pattern += r" - (\d{1,2}:\d{2}) (PM|AM)"
         rm = re.search(pattern, date)
-        
+
         if rm is not None:
             day = rm.group("day")
             time = rm.group("time")
@@ -130,9 +138,10 @@ class StlAldermenSpider(CityScrapersSpider):
     def _parse_end(self, response):
         """Parse end datetime as a naive datetime object. Added by pipeline if None"""
         date = response.css("div.page-title-row p.page-summary::text").get()
-        pattern = r"(?P<day>\d{2}/\d{2}/\d{2}), (\d{1,2}:\d{2}) (PM|AM) - (?P<time>(\d{1,2}:\d{2}) (PM|AM))"
+        pattern = r"(?P<day>\d{2}/\d{2}/\d{2}), (\d{1,2}:\d{2}) (PM|AM)"
+        pattern += r" - (?P<time>(\d{1,2}:\d{2}) (PM|AM))"
         rm = re.search(pattern, date)
-        
+
         if rm is not None:
             day = rm.group("day")
             time = rm.group("time")
@@ -152,7 +161,7 @@ class StlAldermenSpider(CityScrapersSpider):
         temp = []
         for item in location:
             item = item.replace("\n", "")
-            if item is not "":
+            if item != "":
                 temp.append(item)
         location = temp
         i, location_index, sponsor_index = 0, 0, 0
@@ -169,7 +178,9 @@ class StlAldermenSpider(CityScrapersSpider):
             address = []
             for j in range(location_index + 2, sponsor_index):
                 address.append(location[j])
-            address = " ".join(address).replace("Directions to this address", "").strip()
+            address = (
+                " ".join(address).replace("Directions to this address", "").strip()
+            )
         else:
             name = ""
             address = ""
@@ -187,7 +198,7 @@ class StlAldermenSpider(CityScrapersSpider):
             temp_links = []
             link = row.css("a::attr(href)").getall()
             description = row.css("td *::text").getall()
-            description = "".join(description).replace("\n"," ")
+            description = "".join(description).replace("\n", " ")
 
             pattern_mmddyy = r"(?P<date>(\d{1,2}-\d{1,2}-\d{2}))"
             pattern_mmddyyyy = r"(?P<date>(\d{1,2}-\d{1,2}-\d{4}))"
@@ -196,7 +207,7 @@ class StlAldermenSpider(CityScrapersSpider):
             rm_mmddyy = re.search(pattern_mmddyy, description)
             rm_mmddyyyy = re.search(pattern_mmddyyyy, description)
             rm_monthddyyyy = re.search(pattern_monthddyyyy, description)
-            
+
             dt = None
             if rm_mmddyy is not None:
                 date = rm_mmddyy.group("date")
@@ -211,8 +222,12 @@ class StlAldermenSpider(CityScrapersSpider):
             if dt is not None:
                 formatted_date = datetime.strftime(dt, "%m-%d-%y")
                 if len(link) >= 2:
-                    temp_links.append({"href": response.urljoin(link[1]), "title": "Agenda"})
+                    temp_links.append(
+                        {"href": response.urljoin(link[1]), "title": "Agenda"}
+                    )
                 if len(link) == 3:
-                    temp_links.append({"href": response.urljoin(link[2]), "title": "Minutes"})
-                
+                    temp_links.append(
+                        {"href": response.urljoin(link[2]), "title": "Minutes"}
+                    )
+
                 self.agenda_map[formatted_date] = temp_links
--- tests/test_stl_aldermen.py
+++ tests/test_stl_aldermen.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from os.path import dirname, join
 
-import pytest
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -10,77 +9,84 @@ from city_scrapers.spiders.stl_aldermen import StlAldermenSpider
 
 test_response = file_response(
     join(dirname(__file__), "files", "stl_aldermen.html"),
-    url="https://www.stlouis-mo.gov/government/departments/aldermen/",
+    url="https://www.stlouis-mo.gov/events/eventdetails.cfm?Event_ID=26489",
 )
+
+test_detail_response = file_response(
+    join(dirname(__file__), "files", "stl_aldermen_detail.html"),
+    url="https://www.stlouis-mo.gov/events/eventdetails.cfm?Event_ID=26489",
+)
+
+
 spider = StlAldermenSpider()
 
 freezer = freeze_time("2020-07-09")
 freezer.start()
-
-parsed_items = [item for item in spider.parse(test_response)]
-
+spider._parse_links(test_detail_response)
+item = spider._parse_event(test_response)
 freezer.stop()
 
 
-def test_tests():
-    print("Please write some tests for this spider or at least disable this one.")
-    assert False
+def test_title():
+    assert item["title"] == "Full Board of Aldermen"
 
 
-"""
-Uncomment below
-"""
-
-# def test_title():
-#     assert parsed_items[0]["title"] == "EXPECTED TITLE"
+def test_description():
+    assert item["description"] == ""
 
 
-# def test_description():
-#     assert parsed_items[0]["description"] == "EXPECTED DESCRIPTION"
+def test_start():
+    assert item["start"] == datetime(2020, 7, 8, 1, 0)
 
 
-# def test_start():
-#     assert parsed_items[0]["start"] == datetime(2019, 1, 1, 0, 0)
+def test_end():
+    assert item["end"] == datetime(2020, 7, 8, 3, 0)
 
 
-# def test_end():
-#     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
+def test_id():
+    assert item["id"] == "stl_aldermen/202007080100/x/full_board_of_aldermen"
 
 
-# def test_time_notes():
-#     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
+def test_status():
+    assert item["status"] == "passed"
 
 
-# def test_id():
-#     assert parsed_items[0]["id"] == "EXPECTED ID"
+def test_location():
+    assert item["location"] == {
+        "name": "Webinar",
+        "address": "",
+    }
 
 
-# def test_status():
-#     assert parsed_items[0]["status"] == "EXPECTED STATUS"
+def test_source():
+    assert (
+        item["source"]
+        == "https://www.stlouis-mo.gov/events/eventdetails.cfm?Event_ID=26489"
+    )
 
 
-# def test_location():
-#     assert parsed_items[0]["location"] == {
-#         "name": "EXPECTED NAME",
-#         "address": "EXPECTED ADDRESS"
-#     }
+def test_links():
+    assert item["links"] == [
+        {
+            "href": (
+                "https://www.stlouis-mo.gov/internal-apps/"
+                "legislative/upload/agenda/7-8-20 AGENDA 14 2.pdf"
+            ),
+            "title": "Agenda",
+        },
+        {
+            "href": (
+                "https://www.stlouis-mo.gov/internal-apps/"
+                "legislative/upload/minute/BOA Minutes 7-8-20 Updated2.pdf"
+            ),
+            "title": "Minutes",
+        },
+    ]
 
 
-# def test_source():
-#     assert parsed_items[0]["source"] == "EXPECTED URL"
+def test_classification():
+    assert item["classification"] == BOARD
 
 
-# def test_links():
-#     assert parsed_items[0]["links"] == [{
-#       "href": "EXPECTED HREF",
-#       "title": "EXPECTED TITLE"
-#     }]
-
-
-# def test_classification():
-#     assert parsed_items[0]["classification"] == NOT_CLASSIFIED
-
-
-# @pytest.mark.parametrize("item", parsed_items)
-# def test_all_day(item):
-#     assert item["all_day"] is False
+def test_all_day():
+    assert item["all_day"] is False
