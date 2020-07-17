e69daabe946c0e218c0bb3dd4d663f79bfcc2b9f
Leda Liang <ledaliang@wustl.edu>
2020-07-17 16:23:03 -0500

    stl preservation spider

--- city_scrapers/spiders/stl_preservation.py
+++ city_scrapers/spiders/stl_preservation.py
@@ -1,4 +1,9 @@
-from city_scrapers_core.constants import NOT_CLASSIFIED
+import re
+from collections import defaultdict
+from datetime import datetime
+
+import scrapy
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
@@ -7,73 +12,196 @@ class StlPreservationSpider(CityScrapersSpider):
     name = "stl_preservation"
     agency = "St. Louis Preservation Board"
     timezone = "America/Chicago"
-    start_urls = ["https://www.stlouis-mo.gov/government/departments/planning/cultural-resources/preservation-board/index.cfm"]
+    custom_settings = {"ROBOTSTXT_OBEY": False, "CONCURRENT_REQUESTS":1}
+    
+    def __init__(self, *args, **kwargs):
+        self.agenda_map = defaultdict(list)
+        super().__init__(*args, **kwargs)
+
+    def start_requests(self):
+        url = "https://www.stlouis-mo.gov/government/departments/planning/documents/index.cfm"
+        yield scrapy.Request(url=url, method="GET", callback=self._get_agenda_urls, priority=100)
+        
+        calendar_urls = [
+            (
+                "https://www.stlouis-mo.gov/events/"
+                "past-meetings.cfm?department=263&span=-30"
+            ),
+            "https://www.stlouis-mo.gov/events/all-public-meetings.cfm?span=30",
+        ]
+        for url in calendar_urls:
+            yield scrapy.Request(url=url, method="GET", callback=self.parse)
 
     def parse(self, response):
+        for url in self._get_event_urls(response):
+            yield scrapy.Request(url, callback=self._parse_event, dont_filter=True)
+
+    def _get_agenda_urls(self, response):
+        links = response.css("div.CS_Element_PageIndex ul.list-group li a::attr(href)").getall()
+        descriptions = response.css("div.CS_Element_PageIndex ul.list-group li a::text").getall()
+        count = 0
+        urls = []
+        for link, description in zip(links, descriptions):
+            if "preservation" in description.lower() and "agenda" in description.lower():
+                urls.append(response.urljoin(link))
+                count+=1
+            if count > 5:
+                break
+        for url in urls:
+            yield scrapy.Request(url=url, method="GET", callback=self._parse_links, priority=100)
+
+    def _get_event_urls(self, response):
+        event_urls = response.css("ul.list-group h4 a::attr(href)").getall()
+        event_sponsors = response.css("ul.list-group li span.small::text").getall()
+        urls = []
+        for url, sponsor in zip(event_urls, event_sponsors):
+            if "preservation" in sponsor.lower():
+                urls.append(response.urljoin(url))
+        return urls
+
+    def _parse_event(self, response):
         """
-        `parse` should always `yield` Meeting items.
-
-        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
-        needs.
+        return Meeting items.
         """
-        for item in response.css(".meetings"):
-            meeting = Meeting(
-                title=self._parse_title(item),
-                description=self._parse_description(item),
-                classification=self._parse_classification(item),
-                start=self._parse_start(item),
-                end=self._parse_end(item),
-                all_day=self._parse_all_day(item),
-                time_notes=self._parse_time_notes(item),
-                location=self._parse_location(item),
-                links=self._parse_links(item),
-                source=self._parse_source(response),
-            )
+        start = self._parse_start(response)
+        links_key = datetime.strftime(start, "%m-%d")
 
-            meeting["status"] = self._get_status(meeting)
-            meeting["id"] = self._get_id(meeting)
+        meeting = Meeting(
+            title=self._parse_title(response),
+            description="",
+            classification=BOARD,
+            start=start,
+            end=self._parse_end(response),
+            all_day=False,
+            location=self._parse_location(response),
+            source=response.url,
+        )
 
-            yield meeting
+        if links_key in self.agenda_map.keys():
+            meeting["links"] = self.agenda_map[links_key]
+        else:
+            meeting["links"] = []
 
-    def _parse_title(self, item):
+        meeting["status"] = self._get_status(meeting)
+        meeting["id"] = self._get_id(meeting)
+        return meeting
+
+    def _parse_title(self, response):
         """Parse or generate meeting title."""
-        return ""
+        title = response.css("div.page-title-row h1::text").get()
+        title = title.replace("Meeting","")
+        return title.strip()
 
-    def _parse_description(self, item):
-        """Parse or generate meeting description."""
-        return ""
-
-    def _parse_classification(self, item):
-        """Parse or generate classification from allowed options."""
-        return NOT_CLASSIFIED
-
-    def _parse_start(self, item):
+    def _parse_start(self, response):
         """Parse start datetime as a naive datetime object."""
-        return None
+        date = response.css("div.page-title-row p.page-summary::text").get()
+        pattern = r"(?P<day>\d{2}/\d{2}/\d{2}), (?P<time>(\d{1,2}:\d{2}) (PM|AM))"
+        pattern += r" - (\d{1,2}:\d{2}) (PM|AM)"
+        rm = re.search(pattern, date)
 
-    def _parse_end(self, item):
+        if rm is not None:
+            day = rm.group("day")
+            time = rm.group("time")
+            dt = day + " " + time
+            start = datetime.strptime(dt.strip(), "%m/%d/%y %H:%M %p")
+            return start
+        else:
+            return None
+
+    def _parse_end(self, response):
         """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        return None
+        date = response.css("div.page-title-row p.page-summary::text").get()
+        pattern = r"(?P<day>\d{2}/\d{2}/\d{2}), (\d{1,2}:\d{2}) (PM|AM)"
+        pattern += r" - (?P<time>(\d{1,2}:\d{2}) (PM|AM))"
+        rm = re.search(pattern, date)
 
-    def _parse_time_notes(self, item):
-        """Parse any additional notes on the timing of the meeting"""
-        return ""
+        if rm is not None:
+            day = rm.group("day")
+            time = rm.group("time")
+            dt = day + " " + time
+            end = datetime.strptime(dt.strip(), "%m/%d/%y %H:%M %p")
+            return end
+        else:
+            return None
 
-    def _parse_all_day(self, item):
-        """Parse or generate all-day status. Defaults to False."""
-        return False
-
-    def _parse_location(self, item):
+    def _parse_location(self, response):
         """Parse or generate location."""
+        location = response.css("div.col-md-4 div.content-block p *::text").getall()
+        temp = []
+        for item in location:
+            item = item.replace("\n", "")
+            if item != "":
+                temp.append(item)
+        location = temp
+        i, location_index, sponsor_index = 0, 0, 0
+        while i < len(location):
+            if "location" in location[i].lower():
+                location_index = i
+            if "sponsor" in location[i].lower():
+                sponsor_index = i
+                break
+            i += 1
+
+        if location_index + 1 < len(location) and sponsor_index < len(location):
+            name = location[location_index + 1]
+            address = []
+            for j in range(location_index + 2, sponsor_index):
+                address.append(location[j])
+            address = (
+                " ".join(address).replace("Directions to this address", "").strip()
+            )
+            address = address.replace(", ", ",").replace(",", ", ")
+        else:
+            name = ""
+            address = ""
+
         return {
-            "address": "",
-            "name": "",
+            "address": address,
+            "name": name,
         }
 
-    def _parse_links(self, item):
+    def _parse_links(self, response):
         """Parse or generate links."""
-        return [{"href": "", "title": ""}]
+        agency = response.css("div.cs_control h1::text").get()
+        links = response.css("div.download li a::attr(href)").getall()
+        descriptions = response.css("div.download li a::text").getall()
 
-    def _parse_source(self, response):
-        """Parse or generate source."""
-        return response.url
+        pattern_mmddyyyy = r"(?P<date>\d{1,2}-\d{1,2}-\d{4})"
+        pattern_mmddyy = r"(?P<date>\d{1,2}-\d{1,2}-\d{2})"
+        pattern_monthddyyyy = r"(?P<date>[A-Z][a-z]* \d{1,2}, \d{4})"
+        pattern_ddmonthyyyy = r"(?P<date>\d{1,2} [A-Z][a-z]* \d{4})"
+
+        patterns = {
+            "mmddyyyy": [pattern_mmddyyyy, "%m-%d-%Y"],
+            "mmddyy": [pattern_mmddyy, "%m-%d-%y"],
+            "monthddyyyy": [pattern_monthddyyyy, "%B %d, %Y"],
+            "ddmonthyyyy": [pattern_ddmonthyyyy, "%d %B %Y"]
+        }
+        
+        dt = None
+        for pattern in patterns.keys():
+            rm = re.search(patterns[pattern][0], agency)
+            if rm is not None:
+                date = rm.group("date")
+                dt = datetime.strptime(date, patterns[pattern][1])
+                break
+
+        temp_links = []
+        for link, description in zip(links, descriptions):
+            if "agenda" in description.lower():
+                temp_links.append(
+                    {"href": response.urljoin(link), "title": "Agenda"}
+                )
+            elif "presentation" in description.lower():
+                temp_links.append(
+                    {"href": response.urljoin(link), "title": "Presentation"}
+                )
+            elif "minutes" in description.lower():
+                temp_links.append(
+                    {"href": response.urljoin(link), "title": "Minutes"}
+                )
+            else: continue
+        if dt is not None:
+            formatted_date = datetime.strftime(dt, "%m-%d")
+            if formatted_date not in self.agenda_map.keys():
+                self.agenda_map[formatted_date] = temp_links
--- tests/test_stl_preservation.py
+++ tests/test_stl_preservation.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -10,77 +10,85 @@ from city_scrapers.spiders.stl_preservation import StlPreservationSpider
 
 test_response = file_response(
     join(dirname(__file__), "files", "stl_preservation.html"),
-    url="https://www.stlouis-mo.gov/government/departments/planning/cultural-resources/preservation-board/index.cfm",
+    url="https://www.stlouis-mo.gov/events/eventdetails.cfm?Event_ID=26421",
+)
+
+test_detail_response = file_response(
+    join(dirname(__file__), "files", "stl_preservation_detail.html"),
+    url="https://www.stlouis-mo.gov/events/eventdetails.cfm?Event_ID=26421",
 )
 spider = StlPreservationSpider()
 
 freezer = freeze_time("2020-07-15")
 freezer.start()
 
-parsed_items = [item for item in spider.parse(test_response)]
+spider._parse_links(test_detail_response)
+item = spider._parse_event(test_response)
 
 freezer.stop()
 
 
-def test_tests():
-    print("Please write some tests for this spider or at least disable this one.")
-    assert False
+def test_title():
+    assert item["title"] == "Preservation Board"
 
 
-"""
-Uncomment below
-"""
-
-# def test_title():
-#     assert parsed_items[0]["title"] == "EXPECTED TITLE"
+def test_description():
+    assert item["description"] == ""
 
 
-# def test_description():
-#     assert parsed_items[0]["description"] == "EXPECTED DESCRIPTION"
+def test_start():
+    assert item["start"] == datetime(2020, 6, 29, 4, 0)
 
 
-# def test_start():
-#     assert parsed_items[0]["start"] == datetime(2019, 1, 1, 0, 0)
+def test_end():
+    assert item["end"] == datetime(2020, 6, 29, 7, 0)
 
 
-# def test_end():
-#     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
+def test_id():
+    assert item["id"] == "stl_preservation/202006290400/x/preservation_board"
 
 
-# def test_time_notes():
-#     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
+def test_status():
+    assert item["status"] == "passed"
 
 
-# def test_id():
-#     assert parsed_items[0]["id"] == "EXPECTED ID"
+def test_location():
+    assert item["location"] == {
+        "name": "Abrams Building",
+        "address": "1520 MARKET ST. #2000 St. Louis, MO 63103",
+    }
 
 
-# def test_status():
-#     assert parsed_items[0]["status"] == "EXPECTED STATUS"
+def test_source():
+    assert (
+        item["source"]
+        == "https://www.stlouis-mo.gov/events/eventdetails.cfm?Event_ID=26421"
+    )
 
 
-# def test_location():
-#     assert parsed_items[0]["location"] == {
-#         "name": "EXPECTED NAME",
-#         "address": "EXPECTED ADDRESS"
-#     }
+def test_links():
+    assert item["links"] == [
+        {
+            "href": (
+                "https://www.stlouis-mo.gov/government/departments/planning/"
+                "cultural-resources/documents/upload/"
+                "FINAL-AGENDA-WITH-ZOOM-PAGE-6-29-20-3.pdf"
+            ),
+            "title": "Agenda",
+        },
+        {
+            "href": (
+                "https://www.stlouis-mo.gov/government/departments/planning/"
+                "cultural-resources/documents/upload/JUNE-2020-POWERPOINT.pptx"
+            ),
+            "title": "Presentation",
+        },
+    ]
 
 
-# def test_source():
-#     assert parsed_items[0]["source"] == "EXPECTED URL"
+def test_classification():
+    assert item["classification"] == BOARD
 
 
-# def test_links():
-#     assert parsed_items[0]["links"] == [{
-#       "href": "EXPECTED HREF",
-#       "title": "EXPECTED TITLE"
-#     }]
-
-
-# def test_classification():
-#     assert parsed_items[0]["classification"] == NOT_CLASSIFIED
-
-
-# @pytest.mark.parametrize("item", parsed_items)
-# def test_all_day(item):
-#     assert item["all_day"] is False
+def test_all_day():
+    assert item["all_day"] is False
