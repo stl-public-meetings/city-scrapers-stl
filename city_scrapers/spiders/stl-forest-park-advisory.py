e06c3948f8b722b699f2f75a8092aed694d8bd58
Michael Joo <mjmikejoo@gmail.com>
2020-08-12 21:47:48 +0900

    stl fpab almost done

--- city_scrapers/spiders/stl-forest-park-advisory.py
+++ city_scrapers/spiders/stl-forest-park-advisory.py
@@ -15,9 +15,16 @@ class StlForestParkAdvisorySpider(CityScrapersSpider):
 
     def __init__(self, *args, **kwargs):
         self.agenda_map = defaultdict(list)
+        self.minute_map = defaultdict(list)
         super().__init__(*args, **kwargs)
     
     def start_requests(self):
+        agenda_url = (
+            "https://sites.google.com/a/stlouis-mo.gov/forest-park-master-plan/home"
+        )
+        yield scrapy.Request(url=agenda_url, method="GET", callback=self._get_agenda_urls, priority=100)
+        yield scrapy.Request(url=agenda_url, method="GET", callback=self._get_minute_urls) 
+
         meeting_urls = [
             (
                 "https://www.stlouis-mo.gov/government/departments"
@@ -25,17 +32,139 @@ class StlForestParkAdvisorySpider(CityScrapersSpider):
             ),
             (
                 "https://www.stlouis-mo.gov/events/past-meetings.cfm"
-                "?meetingType=All+Meetings&department=117&span=-60"
+                "?meetingType=All+Meetings&department=117"
             ),
         ]
         for url in meeting_urls:
-            yield scrapy.Request(url=url, method="GET", callback=self.parse, priority=100)
+            yield scrapy.Request(url=url, method="GET", callback=self.parse)
         
-        agenda_url = (
-            "https://sites.google.com/a/stlouis-mo.gov/forest-park-master-plan/home"
-        )
-        yield scrapy.Request(url=agenda_url, method="GET", callback=self._get_agenda_urls)
-
     def parse(self, response):
+        for url in self._get_event_urls(response):
+            yield scrapy.Request(url=url, callback=self._parse_event)
+
+    def _get_event_urls(self, response):
+        event_titles = response.xpath('//*/a[contains(@href, "Event_ID")]/text()').getall()')
+        event_urls = response.xpath('//*/a[contains(@href, "Event_ID")]/@href').getall()
+        event_sponsors = response.xpath('//*/span[@class="small" and contains(text(), "|")]/text()').getall()
+        urls = []
+        for url, sponsor, title in zip(event_urls, event_sponsors, event_titles):
+            if "department of parks, recreation and forestry" in sponsor.lower() and "Forest Park Advisory" in title:
+                urls.append(response.urljoin(url))
+        return urls
+    
+    def _parse_event(self, response):
+
+        start = self._parse_start(response)
+        links_key = datetime.strftime(start, "%Y-%m-%d")
+
+        meeting = Meeting(
+            title=self._parse_title(response),
+            description = self._parse_desc(response),
+            classification=BOARD,
+            start=start,
+            end=self._parse_end(response),
+            all_day=False,
+            location=self._parse_location(response),
+            source=response.url,
+        )
+
+        meeting["links"] = []
+
+        if links_key in self.agenda_map.keys():
+            meeting["links"].append(self.agenda_map[links_key])
+
+        if links_key in self.minute_map.keys():
+            meeting["links"].append(self.minute_map[links_key])
+    
+        return meeting
+    
+    def _parse_start(self, response):
+        date = response.xpath('//*/p[@class="page-summary"]/text()').get()
+        date = date.replace('\n', '')
+        pattern = r"(?P<day>\d{2}/\d{2}/\d{2}), (?P<time>(\d{1,2}:\d{2}) (PM|AM))"
+        pattern += r" - (\d{1,2}:\d{2}) (PM|AM)"
+        rm = re.search(pattern, date)
+
+        if rm is not None:
+            day = rm.group("day")
+            time = rm.group("time")
+            dt = day + " " + time
+            start = datetime.strptime(dt.strip(), "%m/%d/%y %H:%M %p")
+            return start
+        else:
+            return None
+    
+    def _parse_title(self, response):
+        title = response.xpath('//*/div[@class="EventTitle"]/text()').get()
+        title = title.replace('\n', '')
+        meeting_type = response.xpath('//*/div[@class="MeetingType"]/text()').get()
+        meeting_type = meeting_type.replace('\n', '')
+
+        return title + ' ' + meeting_type
+    
+    def _parse_desc(self, response):
+        descs = response.xpath('//*/div[@id="EventDisplayBlock"]/div[@class="row"]/div/p/text()').getall()
+        description = ''
+        for desc in descs:
+            if len(desc) > 25:
+                description = description + desc.replace('\xa0', '') + ' '
+
+        return description
+
+    def _parse_end(self, response):
+        date = response.xpath('//*/p[@class="page-summary"]/text()').get()
+        date = date.replace('\n', '')
+        pattern = r"(?P<day>\d{2}/\d{2}/\d{2}), (\d{1,2}:\d{2}) (PM|AM))"
+        pattern += r" - (?P<time>\d{1,2}:\d{?P<time>(2}) (PM|AM)"
+        rm = re.search(pattern, date)
+
+        if rm is not None:
+            day = rm.group("day")
+            time = rm.group("time")
+            dt = day + " " + time
+            start = datetime.strptime(dt.strip(), "%m/%d/%y %H:%M %p")
+            return start
+        else:
+            return None
+    
+    def _parse_location(self, response):
+        title = response.xpath('//*/div[@class="EventTitle"]/text()').get()
+        meeting_type = response.xpath('//*/div[@class="MeetingType"]/text()').get()
+
+        if 'zoom' in title.lower() or 'zoom' in meeting_type.lower():
+            name = "Zoom"
+            address = ""
+        else:
+            infos = response.xpath('//*/strong[text()="Location:"]/parent::p/text()').getall()
+            address = infos[-2].replace('\n', '') + ', ' + infos[-1].replace('\n', '')
+            name = infos[-3].replace('\n', '')
         
+        return {
+            "address": address,
+            "name": name
+        }
+
+    def _get_agenda_urls(self, response):
+        links = response.xpath('//*/a[contains(@href, "FPAB") and contains(@href, "Agenda")]/@href').getall()
+        titles = response.xpath('//*/span[@class="td-value" and contains(text(), "FPAB Agenda")]/text()').getall()
+
+        for link, title in zip(links, titles):
+            if "CANCELLED" not in title:
+                url = response.urljoin(link)
+                date = re.search("([0-9]{4}\-[0-9]{2}\-[0-9]{2})", title)[0]
+                link_obj = []
+                link_obj.append({"href": url, "title": title})
+                self.agenda_map[date] = link_obj
+
+    def _get_minute_urls(self, response):
+        links = response.xpath('//*/a[contains(@href, "FPAB") and contains(@href, "Minutes")]/@href').getall()
+        titles = response.xpath('//*/span[@class="td-value" and contains(text(), "FPAB Minutes")]/text()').getall()
+
+        for link, title in zip(links, titles):
+            if "NONE" not in title:
+                url = response.urljoin(link)
+                date = re.search("([0-9]{4}\-[0-9]{2}\-[0-9]{2})", title)[0]
+                link_obj = []
+                link_obj.append({"href": url, "title": title})
+                self.agenda_map[date] = link_obj
 
