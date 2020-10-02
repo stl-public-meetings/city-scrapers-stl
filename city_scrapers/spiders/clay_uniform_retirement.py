22033a67e87a0a38eb9696fccc498afc35e0f92b
hemantsirsat <hemantsirsat40@gmail.com>
2020-10-02 14:26:16 +0530

    all test successfull

---
+++ city_scrapers/spiders/clay_uniform_retirement.py
@@ -0,0 +1,93 @@
+from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from datetime import datetime
+
+class ClayUniformRetirementSpider(CityScrapersSpider):
+    name = "clay_uniform_retirement"
+    agency = "Clayton Uniformed Emlpoyees Retirement Board"
+    timezone = "America/Chicago"
+    start_urls = ["https://www.claytonmo.gov/government/boards-and-commissions/uniformed-employees-retirement-board"]
+    address = 'Missouri'
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        for item in response.css("table"):
+            meeting = Meeting(
+                title=self._parse_title(response),
+                description=self._parse_description(item),
+                classification=self._parse_classification(item),
+                start=self._parse_start(response),
+                end=self._parse_end(item),
+                all_day=self._parse_all_day(item),
+                time_notes=self._parse_time_notes(item),
+                location=self._parse_location(item),
+                links=self._parse_links(item),
+                source=self._parse_source(response),
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+            yield meeting
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        #t = []
+        title = item.css("tbody tr td a::text").extract_first()
+        #for names in title:
+            #t.append(names)
+        return title
+
+    def _parse_description(self, item):
+        #not available
+        """Parse or generate meeting description."""
+        return ""
+
+    def _parse_classification(self, item):
+        """Parse or generate classification from allowed options."""
+        return "BOARD"
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        date_time = item.css("tbody tr .mobile_hide::text").extract_first()
+        date_time = datetime.strptime(date_time,"%d/%m/%Y %H:%M %p")
+        return date_time
+
+    def _parse_end(self, item):
+        #not available
+        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
+        return None
+
+    def _parse_time_notes(self, item):
+        #not available
+        """Parse any additional notes on the timing of the meeting"""
+        return ""
+
+    def _parse_all_day(self, item):
+        """Parse or generate all-day status. Defaults to False."""
+        return False
+
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        return {
+            "address": self.address,
+            "name": "",
+        }
+
+    def _parse_links(self, item):
+        """Parse or generate links."""
+        host_link = 'https://www.claytonmo.gov'
+        links = item.css("td span a::attr('href')").extract()
+        l=[]
+        for agenda_minutes in links:
+            l.append({"href":host_link+agenda_minutes,"title":agenda_minutes[33:46]})
+        return l
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
---
+++ tests/test_clay_uniform_retirement.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.clay_uniform_retirement import ClayUniformRetirementSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "clay_uniform_retirement.html"),
+    url="https://www.claytonmo.gov/government/boards-and-commissions/uniformed-employees-retirement-board",
+)
+spider = ClayUniformRetirementSpider()
+
+freezer = freeze_time("2020-10-02")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+print(parsed_items)
+freezer.stop()
+
+'''
+def test_tests():
+    print("Please write some tests for this spider or at least disable this one.")
+    assert False
+'''
+
+"""
+Uncomment below
+"""
+
+def test_title():
+    type(parsed_items)
+    assert parsed_items[0]['title'] == "Uniformed Employees Retirement Board"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2020, 10, 8, 8, 0)
+
+
+#def test_end():
+#    assert parsed_items[0]["end"] == datetime(2020, 10, 8, 10, 0)
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "clay_uniform_retirement/202010080800/x/uniformed_employees_retirement_board"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "tentative"
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "",
+        "address": "Missouri"
+    }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == 'https://www.claytonmo.gov/government/boards-and-commissions/uniformed-employees-retirement-board'
+
+
+# def test_links():
+#     assert parsed_items[0]["links"] == [{
+#       "href": "EXPECTED HREF",
+#       "title": "EXPECTED TITLE"
+#     }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == 'BOARD'
+
+
+# @pytest.mark.parametrize("item", parsed_items)
+# def test_all_day(item):
+#     assert item["all_day"] is False
+
+
